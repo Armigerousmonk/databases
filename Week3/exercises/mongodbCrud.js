3c722cab971524fd243fc7e840e4a0d855e76444
Amirhossein <ahshadravan@gmail.com>
2021-02-27 15:32:25 +0330

    Code Along started and added

---
+++ Week3/exercises/mongodbCrud.js
@@ -0,0 +1,10 @@
+/* 1. Install MangoDb
+2. open a command line and go to mangodb file's folder 
+2.1 Download mongoImport from tool section of site and copy it to /bin folder of mangodb directory
+3. copy cluster link from atlas and copy it to the command line
+4. use password to connect to MangoDB
+5. Copy csv file to folder /bin in mangodb directory 
+6. Again goes to mongodb folder in command line and Use command: mongoimport -d city -c test --type csv --file city.csv --headerline 
+
+*/
+
